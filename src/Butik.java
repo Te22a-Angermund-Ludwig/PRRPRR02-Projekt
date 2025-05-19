68c00a8ac8f191ef35bae391eb2eb0fbb9c8b869
Ludwig Angermund <Ludwig.angermund@elev.ga.ntig.se>
2025-05-19 15:34:07 +0200

    skapat en grundläggande butik

--- src/Butik.java
+++ src/Butik.java
@@ -1,10 +1,28 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Butik extends PassivRuta{
     public ArrayList<Föremål> sortiment;
+    public Scanner sc = new Scanner(System.in);
 
-    public void köp(){
+    public void butik(){
+        for (int i = 0; i < sortiment.size(); i++) {
+            System.out.println(i + ". " + sortiment.get(i));
+        }
+        System.out.println("Vilket föremål vill du köpa? (1-7)");
+        String answer = sc.nextLine();
+        switch (answer){
+            case "1":
 
+                break;
+
+        }
+    }
+
+    public void köp(int antal, Föremål föremål, ArrayList<Föremål> ryggsäck){
+        sortiment.remove(föremål);
+        ryggsäck.add(föremål);
+        antal--;
     }
 
     public void lämna(){
